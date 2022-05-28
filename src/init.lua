c5e48d77e1b13df28c6473cf8f8da36e9f3d1350
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2022-05-27 20:50:58 -0400

    Sanity Check Update #1 (0.2)

--- src/init.lua
+++ src/init.lua
@@ -1,22 +1,25 @@
-local GECK = { VERSION = "0.1" }
+local GECK = { VERSION = "0.2" }
 
 function GECK.MakeTool(TOOL_CONFIG)
     local TOOL = {}
-    local TOOL_INSTANCE = Instance.new("Tool", game.Players.LocalPlayer.Backpack)
+    local TOOL_INSTANCE = Instance.new("Tool")
 
+    --> Properties Inherited from Instance
     if TOOL_CONFIG.Name then
         TOOL["NAME"] = TOOL_CONFIG.Name
-    else
+    elseif not TOOL_CONFIG.Name then
         TOOL["NAME"] = "G.E.C.K Tool"
     end
 
     if TOOL_CONFIG.Parent then
         TOOL["PARENT"] = TOOL_CONFIG.Parent
+    elseif not TOOL_CONFIG.Parent then
+        TOOL["PARENT"] = game.Players.LocalPlayer.Backpack
     end
 
     if TOOL_CONFIG.Archivable then
         TOOL["ARCHIVABLE"] = TOOL_CONFIG.Archivable
-    else
+    elseif not TOOL_CONFIG.Archivable then
         TOOL["ARCHIVABLE"] = true
     end
 
