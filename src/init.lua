3a3cd06330831ecb4a95737b3b0e66c23b3544f5
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2022-05-28 00:41:27 -0400

    `0.5` Add: `GECK.MakePart()` and `GECK:Version()`

--- src/init.lua
+++ src/init.lua
@@ -1,8 +1,12 @@
-local GECK = { VERSION = "0.4" }
+local GECK = { VERSION = "0.5" }
+
+function GECK:Version()
+    return self.VERSION
+end
 
 function GECK.MakeTool(TOOL_CONFIG)
     local TOOL = {}
-    local TOOL_SCRIPT = {};
+    local TOOL_SCRIPT = {}
     local TOOL_INSTANCE = Instance.new("Tool")
 
     if (TOOL_CONFIG.Name and typeof(TOOL_CONFIG.Name) == "string") then
@@ -112,6 +116,96 @@ function GECK.MakeTool(TOOL_CONFIG)
             TOOL_SCRIPT.SCRIPT_SOURCE()
         end)
     end
+end
+
+function GECK.MakePart(PART_CONFIG)
+    local PART = {}
+    local PART_INSTANCE = Instance.new("Part")
+
+    if (PART_CONFIG.Name and typeof(PART_CONFIG.Name) == "string") then
+        PART["NAME"] = PART_CONFIG.Name
+    elseif not PART_CONFIG.Name then
+        PART["NAME"] = "GPart" --> Would name "G.E.C.K Part" but this makes it easier to find. :D
+    end
+
+    if (PART_CONFIG.Parent and typeof(PART_CONFIG.Parent) == "Instance") then
+        PART["PARENT"] = PART_CONFIG.Parent
+    elseif not PART_CONFIG.Parent then
+        PART["PARENT"] = game.Workspace
+    end
+
+    if (PART_CONFIG.Archivable and typeof(PART_CONFIG.Archivable) == "boolean") then
+        PART["ARCHIVABLE"] = PART_CONFIG.Archivable
+    elseif not PART_CONFIG.Archivable then
+        PART["ARCHIVABLE"] = true
+    end
+
+    if (PART_CONFIG.Shape and typeof(PART_CONFIG.Shape) == "Enum") then
+        PART["SHAPE"] = PART_CONFIG.Shape
+    elseif not PART_CONFIG.Shape then
+        PART["SHAPE"] = Enum.PartType.Block
+    end
+
+    if (PART_CONFIG.Anchored and typeof(PART_CONFIG.Anchored) == "boolean") then
+        PART["ANCHORED"] = PART_CONFIG.Anchored
+    elseif not PART_CONFIG.Anchored then
+        PART["ANCHORED"] = true
+    end
+
+    if (PART_CONFIG.BrickColor and typeof(PART_CONFIG.BrickColor) == "BrickColor") then
+        PART["BRICK_COLOR"] = PART_CONFIG.BrickColor
+    elseif not PART_CONFIG.BrickColor then
+        PART["BRICK_COLOR"] = BrickColor.new("White")
+    end
+
+    if (PART_CONFIG.CFrame and typeof(PART_CONFIG.CFrame) == "CFrame") then
+        PART["CFRAME"] = PART_CONFIG.CFrame
+    elseif not PART_CONFIG.CFrame then
+        PART["CFRAME"] = CFrame.new(0, 0, 0)
+    end
+    
+    if (PART_CONFIG.CanCollide and typeof(PART_CONFIG.CanCollide) == "boolean") then
+        PART["CAN_COLLIDE"] = PART_CONFIG.CanCollide
+    elseif not PART_CONFIG.CanCollide then
+        PART["CAN_COLLIDE"] = true
+    end
+
+    if (PART_CONFIG.Color and typeof(PART_CONFIG.Color) == "Color3") then
+        PART["COLOR"] = PART_CONFIG.Color
+    elseif not PART_CONFIG.Color then
+        PART["COLOR"] = Color3.new(1, 1, 1)
+    end
+
+    if (PART_CONFIG.Position and typeof(PART_CONFIG.Position) == "Vector3") then
+        PART["POSITION"] = PART_CONFIG.Position
+    elseif not PART_CONFIG.Position then
+        PART["POSITION"] = Vector3.new(0, 0, 0)
+    end
+
+    if (PART_CONFIG.Size and typeof(PART_CONFIG.Size) == "Vector3") then
+        PART["SIZE"] = PART_CONFIG.Size
+    elseif not PART_CONFIG.Size then
+        PART["SIZE"] = Vector3.new(1, 1, 1)
+    end
+
+    if (PART_CONFIG.Transparency and typeof(PART_CONFIG.Transparency) == "number") then
+        PART["TRANSPARENCY"] = PART_CONFIG.Transparency
+    elseif not PART_CONFIG.Transparency then
+        PART["TRANSPARENCY"] = 0
+    end
+
+    PART_INSTANCE.Name = PART.NAME
+    PART_INSTANCE.Archivable = PART.ARCHIVABLE
+    PART_INSTANCE.Parent = PART.PARENT
+    PART_INSTANCE.Shape = PART.SHAPE
+    PART_INSTANCE.Anchored = PART.ANCHORED
+    PART_INSTANCE.BrickColor = PART.BRICK_COLOR
+    PART_INSTANCE.CFrame = PART.CFRAME
+    PART_INSTANCE.CanCollide = PART.CAN_COLLIDE
+    PART_INSTANCE.Color = PART.COLOR
+    PART_INSTANCE.Position = PART.POSITION
+    PART_INSTANCE.Size = PART.SIZE
+    PART_INSTANCE.Transparency = PART.TRANSPARENCY
 end
 
 return GECK
